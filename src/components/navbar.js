5478a106d6748856cc45a95e826cf3a1a4be781d
Harry <39406883+arijey@users.noreply.github.com>
2021-01-11 22:02:23 +0100

    fixed naming case

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,30 +1,42 @@
 import React from "react"
-import Layout from "../components/layout"
-import { Menu } from "antd"
 import { Link } from "gatsby"
+import { Menu } from "antd"
+import {
+  HomeOutlined,
+  ShoppingOutlined,
+  ReadOutlined,
+  PictureOutlined,
+  SettingOutlined,
+} from "@ant-design/icons"
 
-export default function error() {
+const Navbar = () => {
   return (
-    <Layout>
-      <h1>Error: 404</h1>
-      <h2>This Page doesn't exist. Please try one of the following links:</h2>
-      <Menu theme="light" mode="vertical">
+    <nav>
+      <Menu theme="dark" mode="horizontal">
         <Menu.Item key="1">
+          <HomeOutlined />
           <Link to="/">Home</Link>
         </Menu.Item>
+
         <Menu.Item key="2">
+          <ShoppingOutlined />
           <Link to="/products/">Products</Link>
         </Menu.Item>
         <Menu.Item key="3">
+          <ReadOutlined />
           <Link to="/blog/">Blog</Link>
         </Menu.Item>
         <Menu.Item key="4">
+          <PictureOutlined />
           <Link to="/images/">Image Gallery</Link>
         </Menu.Item>
         <Menu.Item key="5">
+          <SettingOutlined />
           <Link to="/examples/">Examples</Link>
         </Menu.Item>
       </Menu>
-    </Layout>
+    </nav>
   )
 }
+
+export default Navbar
